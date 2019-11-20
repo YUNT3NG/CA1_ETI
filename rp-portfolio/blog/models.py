4d1131f9344f5acdf7393e01574a76bddfeb0446
YUNT3NG <yunt3ng@atlassian.com>
2019-11-20 10:58:12 +0800

    Created Blog

--- rp-portfolio/blog/models.py
+++ rp-portfolio/blog/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Project(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="/img")
-
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
