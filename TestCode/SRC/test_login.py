a70c0aa0260243980242d0d9ec20ecc3dbf947f7
YUNT3NG <yunt3ng@atlassian.com>
2019-11-24 15:46:09 +0800

    Tried testing for post

--- TestCode/SRC/test_login.py
+++ TestCode/SRC/test_login.py
@@ -18,6 +18,17 @@ def login(name, passwrd):
     password.send_keys(Keys.RETURN)
     time.sleep(1)
     return True
+def loginPost(name, passwrd):
+    driver.delete_all_cookies()
+    driver.get("http://localhost:8000/admin/login/?next=/admin/")
+    username = driver.find_element_by_name("username")
+    username.send_keys(name)
+    password = driver.find_element_by_name("password")
+    password.send_keys(passwrd)
+    password.send_keys(Keys.RETURN)
+    time.sleep(1)
+    driver.get("http://localhost:8000/admin/blog/post/add/")
+    return True
 
 def comment_input(author, body):
     driver.get("http://localhost:8000/blog/3/")
@@ -75,6 +86,22 @@ def updatePassword(passwrd1, passwrd2):
     pass2.send_keys(passwrd2)
     pass2.send_keys(Keys.RETURN)
     time.sleep(1)
+
+
+def post_input(title, body, value):
+    post = driver.find_element_by_name("title")
+    post.clear()
+    post.send_keys(title)
+    content = driver.find_element_by_name("body")
+    content.clear()
+    content.send_keys(body)
+    time.sleep(1)
+    category = Select(driver.find_element(By.XPATH, '//*[@id="id_categories"]'))
+    category.select_by_value(value)
+    time.sleep(1)
+    driver.find_element_by_name("_save").click()
+    time.sleep(1)
+    return True
 
 
 @pytest.mark.parametrize("adminpage", [
@@ -171,6 +198,7 @@ def test_createNewUser_NewUsername():
     assert "was added successfully. You may edit it again below." in driver.page_source
     time.sleep(1)
 
+#invalidNU
 def test_createNU_ExistingUsername():
     driver.get("http://localhost:8000/admin/auth/user/add/")
     time.sleep(1)
@@ -181,7 +209,7 @@ def test_createNU_ExistingUsername():
     time.sleep(1)
     
     
-
+#invalidPW
 def test_createInvalidPW_NotSame():
     driver.get("http://localhost:8000/admin/auth/user/add/")
     time.sleep(1)
@@ -218,3 +246,35 @@ def test_createInvalidPW_Numerical():
     assert "Please correct the error below." in driver.page_source
     time.sleep(1)
     driver.close()
+
+#doesnt work
+'''
+def test_create_valid_post():
+    loginPost("yunteng", "19V38r00")
+    post_input("Test Post 1", "ETI", 1)
+    time.sleep(1)
+    assert "success" in driver.page_source
+'''
+
+#create an invalid post
+#doesnt register index for category 
+'''
+def test_invalid_title_exists():
+    login("yunteng", "19V38r00")
+    post_input("Django", "abcdefg", 1)
+    assert "error" in driver.page_source
+
+def test_invalid_blank_title():
+    login("yunteng", "19V38r00")
+    post_input("", "abcdefg", 1)
+    assert "error" in driver.page_source
+
+def test_invalid_blank_body():
+    login("yunteng", "19V38r00")
+    post_input("Blankbody", "", 1)
+    assert "error" in driver.page_source
+
+def test_all_blank():
+    login("yunteng", "19V38r00")
+    post_input("", "", 1)
+    assert "error" in driver.page_source'''
